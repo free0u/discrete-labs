7cdd94d720407195ee0e8d746858f55cae78e60d
Anton Evdokimov <free0u@gmail.com>
2012-10-17 00:38:10 +0400

    Solve task A

--- 3-2-undirected-dfs/A/solve.cpp
+++ 3-2-undirected-dfs/A/solve.cpp
@@ -1,3 +1,4 @@
+#pragma comment(linker, "/STACK:31000000")
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
@@ -7,7 +8,24 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "components"
+
+vector< vector<int> > g;
+vector<bool> used;
+vector<int> ans;
+int num;
+
+void dfs(int v) {
+	used[v] = true;
+	ans[v] = num;
+	for (int i = 0; i < g[v].size(); ++i) {
+		int to = g[v][i];
+		if (!used[to]) {
+			dfs(to);
+		}
+	}
+}
+
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +36,32 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	int n, m;
+	cin >> n >> m;
 
+	g.resize(n);
+
+	int x, y;
+	for (int i = 0; i < m; ++i) {
+		scanf("%d %d", &x, &y);
+		--x; --y;
+		g[x].push_back(y);
+		g[y].push_back(x);
+	}
+
+	used.assign(n, false);
+	ans.assign(n, 0);
+	num = 0;
+	for (int i = 0; i < n; ++i) {
+		if (!used[i]) {
+			++num;
+			dfs(i);
+		}
+	}
+
+	cout << num << endl;
+	for (int i = 0; i < n; ++i) {
+		printf("%d ", ans[i]);
+	}
 	return 0;
 }

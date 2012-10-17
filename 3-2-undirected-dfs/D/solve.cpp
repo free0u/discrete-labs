0c5971b7a7237503752591d9380b9531c6115f48
Anton Evdokimov <free0u@gmail.com>
2012-10-17 20:33:31 +0400

    Solve task B

--- 3-2-undirected-dfs/D/solve.cpp
+++ 3-2-undirected-dfs/D/solve.cpp
@@ -1,3 +1,5 @@
+#pragma comment(linker, "/STACK:31000000")
+
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
@@ -7,7 +9,28 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "bipartite"
+
+vector< vector<int> > g;
+vector<bool> used, part;
+
+bool bipartite = true;
+
+void dfs(int v, bool p = false) {
+	part[v] = p;
+	used[v] = true;
+	for (int i = 0; i < g[v].size(); ++i) {
+		int to = g[v][i];
+		if (used[to]) {
+			if (part[v] == part[to]) {
+				bipartite = false;
+			}
+		} else
+		{
+			dfs(to, !p);
+		}
+	}
+}
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +41,24 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	int n, m, x, y;
+	cin >> n >> m;
+	g.resize(n);
 
+	for (int i = 0; i < m; ++i) {
+		scanf("%d %d", &x, &y);
+		--x; --y;
+		g[x].push_back(y);
+		g[y].push_back(x);
+	}
+
+	used.resize(n);
+	part.resize(n);
+	for (int i = 0; i < n; ++i) {
+		if (!used[i]) {
+			dfs(i);
+		}
+	}
+	printf("%s", bipartite ? "YES" : "NO");
 	return 0;
 }

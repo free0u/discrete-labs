f9745c203f235c5bc1a6761019a4b96396ac9aee
Anton Evdokimov <free0u@gmail.com>
2012-10-25 22:27:11 +0400

    Solve task C

--- 3-3-shortest-path/C/solve.cpp
+++ 3-3-shortest-path/C/solve.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "pathsg"
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +18,32 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	const int inf = 1e9 + 31;
+
+	int n, m;
+	cin >> n >> m;
+	vector< vector<int> > g(n, vector<int> (n, inf));
+	int x, y, z;
+	for (int i = 0; i < m; ++i) {
+		scanf("%d %d %d", &x, &y, &z);
+		g[x - 1][y - 1] = z;
+	}
+	for (int i = 0; i < n; ++i) {
+		g[i][i] = 0;
+	}
+	for (int k = 0; k < n; ++k) {
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
+			}
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			printf("%d ", g[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }

557e0b011206f53260710bdbdb93b0984be8988c
Anton Evdokimov <free0u@gmail.com>
2012-11-14 19:49:52 +0400

    Add read and output parts

--- 3-4-spanning-trees/E/solve.cpp
+++ 3-4-spanning-trees/E/solve.cpp
@@ -7,7 +7,23 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "chinese"
+
+struct edge {
+	int from, to, w;
+	
+	edge() {}
+	
+	edge(int from, int to, int w) : from(from), to(to), w(w)
+	{
+	}
+};
+
+typedef vector<edge> graph;
+
+bool find_mst(graph &g, vector<int> &edges_in_mst) {
+	return false;
+}
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +34,29 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	int n, m;
+	cin >> n >> m;
+	graph source_edges;
+	int from, to, w;
+	for (int i = 0; i < m; ++i) {
+		scanf("%d %d %d", &from, &to, &w);
+		from--; to--;
+		source_edges.push_back(edge(from, to, w));
+	}
+
+	vector<int> ind;
+	bool ok = find_mst(source_edges, ind);
+	if (ok) {
+		long long res = 0;
+		for (int i = 0; i < ind.size(); ++i) {
+			res += source_edges[ind[i]].w;
+		}
+		cout << "YES" << endl;
+		cout << res;
+	} else
+	{
+		cout << "NO";
+	}
 
 	return 0;
 }

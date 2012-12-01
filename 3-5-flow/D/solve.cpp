ee012d12ce4174728127a0d2bf634a6be92d4d35
Anton Evdokimov <free0u@gmail.com>
2012-12-01 15:56:30 +0400

    Solve task D (AC)

--- 3-5-flow/D/solve.cpp
+++ 3-5-flow/D/solve.cpp
@@ -1,3 +1,5 @@
+#pragma comment(linker, "/STACK:32000000")
+
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
@@ -7,7 +9,27 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "paths"
+
+vector< vector<int> > g;
+vector<bool> used;
+vector<int> mt;
+
+bool kuhn(int v) {
+	if (used[v]) {
+		return false;
+	}
+	used[v] = true;
+	for (int i = 0; i < g[v].size(); ++i) {
+		int to = g[v][i];
+		if (mt[to] == -1 || kuhn(mt[to])) {
+			mt[to] = v;
+			return true;
+		}
+	}
+
+	return false;
+}
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +40,31 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	int n, m, k;
+	cin >> n >> m;
+
+	g.resize(n);
+
+	int from, to;
+	for (int i = 0; i < m; ++i) {
+		scanf("%d %d", &from, &to);
+		--from;
+		--to;
+		g[from].push_back(to);
+	}
+
+	mt.assign(n, -1);
+	for (int i = 0; i < n; ++i) {
+		used.assign(n, false);
+		kuhn(i);
+	}
+	int ans = 0;
+	for (int i = 0; i < mt.size(); ++i) {
+		if (mt[i] != -1) {
+			++ans;
+		}
+	}
+	cout << n - ans;
 
 	return 0;
 }

86ed1ad4c49573a0c1a001ec56805b1545569fb3
Anton Evdokimov <free0u@gmail.com>
2013-03-02 21:42:29 +0400

    Solve task C

--- 4-1-subword-search/C/solve.cpp
+++ 4-1-subword-search/C/solve.cpp
@@ -7,7 +7,23 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "prefix"
+
+vector<int> prefix_function(string const& s) {
+	int len = s.length();
+	vector<int> res(len);
+	for (int i = 1; i < len; ++i) {
+		int j = res[i - 1];
+		while (j > 0 && s[i] != s[j]) {
+			j = res[j - 1];
+		}
+		if (s[i] == s[j]) {
+			++j;
+		}
+		res[i] = j;
+	}
+	return res;
+}
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +34,13 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	string s;
+	cin >> s;
+
+	vector<int> ans = prefix_function(s);
+	for (int i = 0; i < ans.size(); ++i) {
+		printf("%d ", ans[i]);
+	}
 
 	return 0;
 }

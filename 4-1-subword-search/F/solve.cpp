eb4cbf27003f934428abfbf4bff859215adc20fd
Anton Evdokimov <free0u@gmail.com>
2013-03-02 22:16:30 +0400

    Solve task F

--- 4-1-subword-search/F/solve.cpp
+++ 4-1-subword-search/F/solve.cpp
@@ -7,7 +7,24 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "period"
+
+
+vector<int> prefix_function(string const& s) {
+	int len = s.length();
+	vector<int> res(len);
+	for (int i = 1; i < len; ++i) {
+		int j = res[i - 1];
+		while (j > 0 && s[i] != s[j]) {
+			j = res[j - 1];
+		}
+		if (s[i] == s[j]) {
+			++j;
+		}
+		res[i] = j;
+	}
+	return res;
+}
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +35,15 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	string s;
+	cin >> s;
+
+	vector<int> pi = prefix_function(s);
+
+	int k = s.size() - pi.back();
+
+	printf("%d\n", s.size() % k == 0 ? k : s.size());
+
 
 	return 0;
 }

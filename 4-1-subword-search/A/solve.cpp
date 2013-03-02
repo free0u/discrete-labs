d0bfc1c36f31125b2e9b9caba73a2bd4f4c214c2
Anton Evdokimov <free0u@gmail.com>
2013-03-02 21:53:17 +0400

    Solve task A

--- 4-1-subword-search/A/solve.cpp
+++ 4-1-subword-search/A/solve.cpp
@@ -7,7 +7,23 @@
 #include <algorithm>
 
 using namespace std;
-#define task_name ""
+#define task_name "search1"
+
+vector<int> prefix_function(string const& s) {
+	int len = s.length();
+	vector<int> res(len);
+	for (int i = 1; i < len; ++i) {
+		int j = res[i - 1];
+		while (j > 0 && s[i] != s[j]) {
+			j = res[j - 1];
+		}
+		if (s[i] == s[j]) {
+			++j;
+		}
+		res[i] = j;
+	}
+	return res;
+}
 
 int main() {
 #ifndef HOME_free0u
@@ -18,6 +34,26 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	string s, t;
+	cin >> s >> t;
+	int n = s.length();
+	int m = t.length();
+
+	s += '#';
+	s += t;
+	vector<int> pi = prefix_function(s);
+
+	vector<int> ans;
+	for (int i = 0; i < pi.size(); ++i) {
+		if (pi[i] == n) {
+			ans.push_back(i - n - n + 1);
+		}
+	}
+
+	cout << ans.size() << endl;
+	for (int i = 0; i < ans.size(); ++i) {
+		printf("%d ", ans[i]);
+	}
 
 	return 0;
 }

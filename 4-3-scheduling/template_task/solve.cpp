da38aa241c86771c83d9a93b78487b7017b66af3
Anton Evdokimov <free0u@gmail.com>
2013-05-27 23:40:46 +0400

    Add template netbeans task

---
+++ 4-3-scheduling/template_task/solve.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define task_name "task_name"
+
+int main() {
+#ifndef HOME_free0u
+	freopen(task_name".in", "r", stdin);
+	freopen(task_name".out", "w", stdout);
+#else
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+        
+        int a, b;
+        cin >> a >> b;
+        cout << a + b;
+
+	return 0;
+}

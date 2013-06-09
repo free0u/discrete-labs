b31760179a4ced80540c1d55e917495e6042f151
Anton Evdokimov <free0u@gmail.com>
2013-06-09 22:46:12 +0400

    Added template qt creator project and simple checker

---
+++ simple_checker/gen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import random
+import sys
+
+N = 2
+print N
+for i in range(N):
+    sys.stdout.write(str(random.randint(1, 5)) + " ")
+print
+
+for i in range(N):
+    sys.stdout.write(str(random.randint(1, 5)) + " ")

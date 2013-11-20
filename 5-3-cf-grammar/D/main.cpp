8f752f9e6029490cc09cba8d9e1802a8ebd86f73
Anton Evdokimov <free0u@gmail.com>
2013-11-20 22:34:32 +0400

    Solved task D (AC)

--- 5-3-cf-grammar/D/main.cpp
+++ 5-3-cf-grammar/D/main.cpp
@@ -1,4 +1,3 @@
-
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
@@ -14,35 +13,36 @@ typedef long long ll;
 
 vector<vector<vector<ll> > > dp;
 
-void out() {
-    for (size_t j = 0; j < dp.size(); ++j) {
-        printf("%c\n", (char)('A' + j));
-        for (size_t i = 0; i < dp[j].size(); ++i) {
-            for (size_t g = 0; g < dp[j][i].size(); ++g) {
-                printf("%2lld ", dp[j][i][g]);
-            }
-            cout << endl;
-        }
-    }
-}
+//void out() {
+//    for (size_t j = 0; j < dp.size(); ++j) {
+//        printf("%c\n", (char)('A' + j));
+//        for (size_t i = 0; i < dp[j].size(); ++i) {
+//            for (size_t g = 0; g < dp[j][i].size(); ++g) {
+//                printf("%2lld ", dp[j][i][g]);
+//            }
+//            cout << endl;
+//        }
+//    }
+//}
 
-ll mod(ll x) {
-    return x % 1000000007;
-}
+//ll mod(ll x) {
+//    return x % 1000000007;
+//}
+
+#define ALP_SIZE 26
 
 int main() {
 #ifndef HOME_free0u
     freopen(task_name".in", "r", stdin);
     freopen(task_name".out", "w", stdout);
-    #define ALP_SIZE 26
 #else
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    #define ALP_SIZE 26
 #endif
 
 
     vector<bool> is_term(ALP_SIZE, false);
+
     string start, s;
     int n;
     cin >> n >> start; getchar();
@@ -52,17 +52,14 @@ int main() {
         char from = s[0];
         is_term[from - 'A'] = true;
         size_t l = s.length();
-        if (l > 4)
-            s = s.substr(5, l - 5);
-        else
-            s = "";
+
+        s = s.substr(5, l - 5);
+        if (s.length() == 2) {
+            is_term[s[0] - 'A'] = true;
+            is_term[s[1] - 'A'] = true;
+        }
         a[from - 'A'].push_back(s);
 
-        for (size_t j = 0; j < s.size(); ++j) {
-            if (s[j] >= 'A' && s[j] <= 'Z') {
-                is_term[s[j] - 'A'] = true;
-            }
-        }
     }
 
     string pattern;
@@ -75,9 +72,8 @@ int main() {
         for (int j = 0; j < ALP_SIZE; ++j) {
             for (size_t g = 0; g < a[j].size(); ++g) {
                 int from = j;
-                string to = a[j][g];
 
-                if (to.length() == 1 && to[0] == pattern[i]) {
+                if (a[j][g].length() == 1 && a[j][g][0] == pattern[i]) {
                     dp[from][i][i]++;
                 }
             }
@@ -86,29 +82,17 @@ int main() {
 
     for (int len_m = 1; len_m <= len; ++len_m) {
         for (int i = 0; i < len; ++i) {
-            //for (int j = i + 1; j < len; ++j) {
-            {
-                int j = i + len_m - 1;
-                if (j >= len) continue;
-//                printf("\ni = %d; j = %d\n", i, j);
-//                out();
+            int j = i + len_m - 1;
+            if (j >= len) continue;
 
-
-                if (j - i == 3) {
-                    int z = 0;
-                    (void)z;
-                }
-
-                for (int d = i; d < j; ++d) {
+            for (int d = i; d < j; ++d) {
                 for (int term = 0; term < ALP_SIZE; ++term) {
+                    if (!is_term[term]) continue;
                     for (size_t k = 0; k < a[term].size(); ++k) {
-                        string to = a[term][k];
-                        if (to.length() != 2) continue;
+                        if (a[term][k].length() != 2) continue;
 
-                        dp[term][i][j] = mod(dp[term][i][j] + (dp[to[0] - 'A'][i][d]) *
-                                                              (dp[to[1] - 'A'][d + 1][j]));
-
-                        }
+                        dp[term][i][j] = (dp[term][i][j] + (dp[a[term][k][0] - 'A'][i][d]) *
+                                                              (dp[a[term][k][1] - 'A'][d + 1][j])) % 1000000007;
                     }
                 }
             }

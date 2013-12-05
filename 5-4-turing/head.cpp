454960aa24208d152709dccf4760add6a213002a
Anton Evdokimov <free0u@gmail.com>
2013-12-05 18:20:58 +0400

    Solved task B (AC)

---
+++ 5-4-turing/head.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define task_name ""
+
+int main() {
+    freopen(task_name".out", "w", stdout);
+
+    vector<string> ans;
